9fb61653f4f186634062d9c4c54d294401d7a982
leejiseop <lkj0245@naver.com>
2025-05-25 23:58:48 +0900

    spring initializer

---
+++ src/main/java/com/example/muroba/MurobaApplication.java
@@ -0,0 +1,13 @@
+package com.example.muroba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurobaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurobaApplication.class, args);
+	}
+
+}

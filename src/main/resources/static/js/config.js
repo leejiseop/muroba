e930cc58ee8ae27f67231b1c8d84e2f1f513504f
leejiseop <lkj0245@naver.com>
2025-12-15 17:27:38 +0900

    test

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -1,3 +1,5 @@
-const origin = `https://muroba.store`;
+// const origin = `https://muroba.store`;
+const origin = `https://d2hdmspkitecs0.cloudfront.net/`;
+
 // const origin = `http://52.78.250.100:8080`;
 // const origin = `http://localhost:8080`;
